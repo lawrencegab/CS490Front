b196e0cf0ef13f21524276096bcc9a89d47bab0e
Abanobmikaeel@gmail.com <beans@users-MBP.fios-router.home>
2018-02-07 23:37:02 -0500

    made some updates

--- grabtest.php
+++ grabtest.php
@@ -1,3 +1,4 @@
 <?php
-print_r($_POST)
+	$data = json_decode(file_get_contents('php://input'), true);
+	var_dump($data)
 ?>
--- post.php
+++ post.php
@@ -1,6 +1,4 @@
 <?php
-
-echo 'False';
-echo 'True';
-
+	echo 'False';
+	echo 'True';
 ?>
